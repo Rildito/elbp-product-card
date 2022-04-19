ce5baf952db4ba9036498609ecb465933376f01c
Enrique <barra.paredes.enrique.luis@gmail.com>
2022-04-19 16:23:24 -0400

    Primer commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# DO-Product-Card
+
+Este es un paquete de pruebas de despliege en NPM
+
+
+## Enrique Luis
+```
+import {ProductCard,ProductImage,ProductTitle, ProductButtons } from 'elbp-product-card';
+```
+```
+ <ProductCard
+        product={product}
+        initialValues={{
+          count: 4,
+          maxCount: 7
+        }}
+      >
+        {
+          ({ reset, isMaxCountReached, count, increaseBy }) => (
+            <>
+              <ProductImage/>
+              <ProductTitle />
+              <ProductButtons />
+            </>
+          )
+        }
+
+      </ProductCard>
+```
---
+++ src/components/ProductTitle.tsx
@@ -0,0 +1,24 @@
+import React, { useContext, CSSProperties } from "react";
+import { ProductContext } from "./ProductCard";
+import styles from '../styles/styles.module.css';
+
+export interface Props {
+    title?: string;
+    className?: string
+    style?: CSSProperties
+}
+
+export const ProductTitle = ({ title, className, style }: Props) => {
+
+    const { product } = useContext(ProductContext);
+    let titleToShow: string;
+
+    if (title) {
+        titleToShow = title;
+    } else {
+        titleToShow = product.title;
+    }
+    return (
+        <span style={style} className={`${styles.productoDescription} ${className}`}>{titleToShow}</span>
+    );
+};
