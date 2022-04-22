9d29ef1a8ac12299813db1f8de7bb74324962e7a
Enrique <barra.paredes.enrique.luis@gmail.com>
2022-04-22 16:28:14 -0400

    Version 0.0.1 lista

---
+++ test/components/ProductImage.test.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { ProductImage, ProductCard } from '../../src/components';
+import { product2 } from '../data/products';
+
+
+
+describe('ProductImage', () => {
+    test('Debe de mostrar el componente correctamente con la imagen personalizada', () => {
+
+        const wrapper = renderer.create(
+            <ProductImage img="https://hola.jpg" className='custom-product-class' />
+        );
+
+        expect(wrapper.toJSON()).toMatchSnapshot();
+
+    });
+
+    test('debe de mostrar el componente con la imagen del producto', () => {
+        const wrapper = renderer.create(
+            <ProductCard product={product2}>
+                {
+                    () => (
+                        <ProductImage/>
+                    )
+                }
+            </ProductCard>
+        );
+
+        expect(wrapper.toJSON()).toMatchSnapshot();
+    });
+});
